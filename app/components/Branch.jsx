ffa7e0b855c386fbe1d106744792afc7b23d1c25
Frankenmint <admin@honeybadgerofmoney.com>
2015-10-26 11:49:45 -0700

    Update Branch.jsx
    Add in Toggle Market Button to appear on markets page

--- app/components/Branch.jsx
+++ app/components/Branch.jsx
@@ -10,40 +10,70 @@ var RouteHandler = Router.RouteHandler;
 var Navigation = Router.Navigation;
 var Link = Router.Link;
 
-var AddMarketModal =  require('./AddMarketModal')
+var AddMarketModal =  require('./AddMarketModal');
 var Markets = require('./Markets');
-
+var MarketActions = require('../actions/MarketActions');
 var constants = require('../libs/constants');
 
 var Branch = React.createClass({
 
   // assuming only one branch and all markets in store are of that branch
-  mixins: [FluxMixin, StoreWatchMixin('market', 'search', 'branch', 'config'), Navigation],
+  mixins: [FluxMixin, StoreWatchMixin('market', 'branch', 'config'), Navigation],
 
   getInitialState: function() {
     return {
       addMarketModalOpen: false,
       marketsPerPage: constants.MARKETS_PER_PAGE,
       visiblePages: 3,
+      showMatures: false, 
+      toggleIcon: "fa fa-toggle-off",
+      toggleInfo: "Swap Between Open and Closed Markets with this Button",
       pageNum: this.props.params.page ? this.props.params.page - 1 : 0
     };
   },
 
   getStateFromFlux: function () {
+
     var flux = this.getFlux();
     var marketState = flux.store('market').getState();
-    var searchState = flux.store('search').getState();
     var currentBranch = flux.store('branch').getCurrentBranch();
     var account = flux.store('config').getAccount();
 
+
+
     return {
-      searchKeywords: searchState.keywords,
-      markets: searchState.results,
+      markets: marketState.markets,
       pendingMarkets: marketState.pendingMarkets,
       currentBranch: currentBranch,
       account: account
     }
   },
+
+toggleMatures: function(event) {
+
+  //All the magic that swaps the icons the toggle state
+  if (!this.state.showMatures) {
+    this.setState({ toggleIcon: 'fa fa-toggle-on' , toggleInfo: 'Mature Mkts', showMatures: true });
+    window.flux.actions.market.mMarkets = true;
+  } 
+  else {
+    this.setState({ toggleIcon: 'fa fa-toggle-off', toggleInfo: 'Open Mkts', showMatures: false });
+    window.flux.actions.market.mMarkets = false;
+  }
+  window.flux.actions.market.loadMarketCache();
+  console.log("Swapped the Open Markets for the Matured Markets");
+  
+  },
+// else {
+//   window.flux.actions.market.mMarkets = false;
+//   window.flux.actions.market.loadMarketCache();
+//   mktToggleOff = true;
+//   this.setState({ toggleIcon: 'fa fa-toggle-on', toggleInfo: 'Load Open Markets' });
+//   console.log("Loading Active Markets");
+
+//     }
+//   },
+
 
   toggleAddMarketModal: function(event) {
 
@@ -56,16 +86,6 @@ var Branch = React.createClass({
     this.setState({ pageNum: data.selected });
   },
 
-  onChangeSearchInput: function (event) {
-    this.setState({ searchKeywords: event.target.value });
-    this.debounceSearchInput(event.target.value);
-  },
-  
-  debounceSearchInput: _.debounce(function (val) {
-    this.handlePageChanged({ selected: 0 });
-    this.getFlux().actions.search.updateKeywords(val);    
-  }, 500),
-  
   render: function () {
 
     var start = 0 + (this.state.pageNum) * this.state.marketsPerPage;
@@ -99,15 +119,7 @@ var Branch = React.createClass({
     return (
       <div id="branch">
         { pendingMarketsSection }
-        <h3 className="clearfix">Markets { submitMarketAction }</h3>
-            
-				<input type="search"
-					className="markets-search-input"
-					value={ this.state.searchKeywords }
-					placeholder="Search"
-					tabIndex="0"
-					onChange={ this.onChangeSearchInput } />
-    					
+        <h3 className="clearfix">Markets <a href="javascript:void(0);" onClick={ this.toggleMatures } ><i className= { this.state.toggleIcon } title = {this.state.toggleInfo} /></a> { submitMarketAction }</h3>
         <div className='subheading clearfix'>
           <span className='showing'>Showing { start+1 } - { end } of { total }</span>
           <Paginate 
@@ -117,7 +129,7 @@ var Branch = React.createClass({
             pageNum={ total / this.state.marketsPerPage }
             marginPagesDisplayed={ 2 }
             pageRangeDisplayed={ 5 }
-            forceSelected={ this.state.pageNum }
+            initialSelected={ this.state.pageNum }
             clickCallback={ this.handlePageChanged }
             containerClassName={ 'paginator' }
             subContainerClassName={ 'pages' }
